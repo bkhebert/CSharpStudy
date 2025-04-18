3c19227d4528627c1607a5a91ef221e4ae2cc19a
bkhebert <bondman2004tippy@gmail.com>
2025-04-18 00:04:28 -0500

    DATATYPES

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,8 +15,24 @@ namespace Giraffe
       name = "Mike";
       Console.WriteLine("He really liked the name " + name);
       Console.WriteLine("But didn't like being " + age);
-      
 
+      string word =  "a string";
+      char character = 'e';
+      int number = 50;
+      float leastAccurate = 30.5F;
+      double middleAccurate = 40.53;
+      decimal mostAccurate = 40.65M;
+      bool truth = true;
+      bool falsy = false;
+      Console.WriteLine("string " + word);
+      Console.WriteLine("int " + number);
+      Console.WriteLine("bool " + truth);
+      Console.WriteLine("bool " + falsy);
+      Console.WriteLine("decimal + ends with M " + mostAccurate);
+      Console.WriteLine("double " + middleAccurate);
+      Console.WriteLine("float ends with F " + leastAccurate);
+      Console.WriteLine("character " + character);
+      
       Console.ReadLine();
     }
   }
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b4bea896c9e8f0b6bff3eb942030a76cac1fc05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+354b8a6ba1aeb325d10ba792e14fd302cbdf7da8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
