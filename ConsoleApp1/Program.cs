d620156b264290ea41e7b7676ce602329b8b746f
bkhebert <bondman2004tippy@gmail.com>
2025-04-17 23:42:48 -0500

    FIRST C SHARP COMMIT

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+namespace Giraffe
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("    /|");
+      Console.WriteLine("   / |");
+      Console.WriteLine("  /  |");
+      Console.WriteLine(" /   |");
+      Console.WriteLine("/____|");
+
+      Console.ReadLine();
+    }
+  }
+}
