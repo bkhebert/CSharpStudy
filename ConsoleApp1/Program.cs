9c6610c9cd4428e64477127dfb3f5d37eeed7219
bkhebert <bondman2004tippy@gmail.com>
2025-04-17 23:49:04 -0500

    George Story

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,11 +7,11 @@ namespace Giraffe
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("    /|");
-      Console.WriteLine("   / |");
-      Console.WriteLine("  /  |");
-      Console.WriteLine(" /   |");
-      Console.WriteLine("/____|");
+      Console.WriteLine("There once was a man named George");
+      Console.WriteLine("He was 70 yrs old");
+      Console.WriteLine("He really liked the name George");
+      Console.WriteLine("But didn't like being 70");
+      
 
       Console.ReadLine();
     }
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d620156b264290ea41e7b7676ce602329b8b746f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
