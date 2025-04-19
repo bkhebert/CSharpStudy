c495461d30e1d835bcd30540a3db71ea8c65d37d
bkhebert <bondman2004tippy@gmail.com>
2025-04-19 18:44:29 -0500

    ADD decimal calculator

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -60,16 +60,22 @@ namespace Giraffe
       // Console.ReadLine();
 
       // WHOLE NUMBER CALCULATOR
+      // Console.Write("Enter a number: ");
+      // int num1 = Convert.ToInt32(Console.ReadLine());
+      // Console.Write("Enter another number: ");
+      // int num2 = Convert.ToInt32(Console.ReadLine());
+      // Console.WriteLine( num1 + num2);
+
+      // Console.ReadLine();
+
+      // DECIMAL NUMBER CALCULATOR
       Console.Write("Enter a number: ");
-      int num1 = Convert.ToInt32(Console.ReadLine());
-      Console.Write("Enter another number: ");
-      int num2 = Convert.ToInt32(Console.ReadLine());
-      Console.WriteLine( num1 + num2);
+      double decinum1 = Convert.ToDouble(Console.ReadLine());
+      Console.Write("Enter another decinumber: ");
+      double decinum2 = Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine( decinum1 + decinum2);
 
       Console.ReadLine();
-
-
-
 
     }
   }
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05d708bb0d7af6e2ab921081d7e59d66fc3e32e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dbcbaf8869af32c7d744bfebd9dfca1eecfbc3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
